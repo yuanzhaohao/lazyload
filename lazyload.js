b8b5b2ffae214245e5681f6fee0745dae7670a57
yuanzhaohao <yuanzhaohao@163.com>
2015-07-26 19:22:26 +0800

    add some utils func

--- lazyload.js
+++ lazyload.js
@@ -17,10 +17,73 @@
 
 var utils = (function (win, doc) {
   var self = {};
+  var ArrayProto = Array.prototype,
+    ObjProto = Object.prototype,
+    FuncProto = Function.prototype;
+  var toString = ObjProto.toString,
+    hasOwnProperty = ObjProto.hasOwnProperty;
+  var nativeKeys = Object.keys,
+    nativeIsArray = Array.isArray;
 
   self.isObject = function (obj) {
     var type = typeof obj;
     return type === 'function' || type === 'object' && !!obj;
+  };
+
+  self.isArray = nativeIsArray || function (obj) {
+    return toString.call(obj) === '[object Array]';
+  };
+
+  self.has = function (obj, key) {
+    return obj != null && hasOwnProperty.call(obj, key);
+  };
+
+  self.keys = function (obj) {
+    if (!self.isObject) {
+      return [];
+    }
+    if (nativeKeys) {
+      nativeKeys(obj);
+    }
+    var keys = [];
+    for (var key in obj) {
+      if (self.has(obj, key)) {
+        keys.push(key);
+      }
+    }
+    return keys;
+  };
+
+  self.extend = function (obj, source) {
+    if (!source) {
+      return obj;
+    }
+    var keys = self.keys(obj),
+      i = keys.length,
+      key;
+    while (i--) {
+      key = keys[i];
+      if (source[key] !== undefined) {
+        obj[key] = source[key];
+      }
+    };
+    return obj;
+  };
+
+  self.each = function (obj, iteratee, context) {
+    var i, length;
+    if (self.isArray(obj)) {
+      for (i = 0, length = obj.length; i < length; i++) {
+        iteratee.call(context, obj[i], i, obj);
+      }
+    }
+    else {
+      var keys = self.keys(obj);
+      for (i = 0, length = keys.length; i < length; i++) {
+        iteratee.call(context, obj[keys[i]], keys[i], obj);
+      }
+    }
+    return obj;
   };
 
   return self;
@@ -39,12 +102,14 @@ Lazyload.prototype = {
     var self = this;
     var defaults = {
       container: document,
-      imgAttr: 'data-lazyload',
+      attr: 'data-lazyload',
       diff: 100,
       autoDestroy: true
     };
-
-    console.log(defaults.container);
+    cfgs = utils.extend(defaults, cfgs);
+    console.log(utils.each(cfgs, function (val, index) {
+      console.log(val, index);
+    }));
   }
 };
 
